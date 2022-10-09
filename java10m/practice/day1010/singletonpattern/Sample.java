758418809d663bab84e7cca9086ffd1b8e2c0fa9
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-10 01:53:02 +0900

    Singleton 두번째 객체 생성

--- java10m/practice/day1010/singletonpattern/Sample.java
+++ java10m/practice/day1010/singletonpattern/Sample.java
@@ -2,6 +2,6 @@ package lionlike.java10m.practice.day1010.singletonpattern;
 
 public class Sample {
     public static void main(String[] args) {
-//        Singleton si = new Singleton(); //  private 오류가 발생
+        Singleton si = Singleton.getInstance(); //  객체 생성
     }
 }
