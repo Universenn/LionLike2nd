6235d9c1e3f6d4cdec1dfb8a7b635c2e9f1463fb
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-10 01:50:18 +0900

    Singleton 첫번째

---
+++ java10m/practice/day1010/singletonpattern/Sample.java
@@ -0,0 +1,7 @@
+package lionlike.java10m.practice.day1010.singletonpattern;
+
+public class Sample {
+    public static void main(String[] args) {
+//        Singleton si = new Singleton(); //  private 오류가 발생
+    }
+}
---
+++ java10m/practice/day1010/singletonpattern/Singleton.java
@@ -0,0 +1,9 @@
+package lionlike.java10m.practice.day1010.singletonpattern;
+
+public class Singleton {
+    private Singleton(){}
+
+//    public static Singleton getInstance(){
+//        return new Singleton(); // 같은 생성자 호출이 가능
+//    }
+}
