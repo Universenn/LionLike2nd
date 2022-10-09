0e3325658682fdc9476c9b6a91b9547ac5d59eed
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-10 01:17:38 +0900

    인터페이스 연습

---
+++ java10m/practice/day1009/interfacee/Animal.java
@@ -0,0 +1,9 @@
+package lionlike.java10m.practice.day1009.interfacee;
+
+class Animal {
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ java10m/practice/day1009/interfacee/ZooKeeper.java
@@ -0,0 +1,8 @@
+package lionlike.java10m.practice.day1009.interfacee;
+
+class ZooKeeper {
+    void feed(Predator prd) {  // 호랑이가 오면 사과를 던져 준다.
+        System.out.println("feed " + prd.getFood());
+    }
+}
+
