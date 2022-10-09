975d960d66535c2ea00672c6040132c0e7db8ac1
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-10 01:58:12 +0900

    Singleton 세번째

--- java10m/practice/day1010/singletonpattern/Sample.java
+++ java10m/practice/day1010/singletonpattern/Sample.java
@@ -2,6 +2,8 @@ package lionlike.java10m.practice.day1010.singletonpattern;
 
 public class Sample {
     public static void main(String[] args) {
-        Singleton si = Singleton.getInstance(); //  객체 생성
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton1 == singleton2);  // true 출력
     }
 }
--- java10m/practice/day1010/singletonpattern/Singleton.java
+++ java10m/practice/day1010/singletonpattern/Singleton.java
@@ -1,9 +1,13 @@
 package lionlike.java10m.practice.day1010.singletonpattern;
 
 public class Singleton {
+    private static Singleton one;
     private Singleton(){}
 
     public static Singleton getInstance(){
-        return new Singleton(); // 같은 생성자 호출이 가능
+        if(one ==null){
+            one = new Singleton();
+        }
+        return one; // 같은 생성자 호출이 가능
     }
 }
