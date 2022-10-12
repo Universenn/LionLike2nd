fc5f3cebfe720ed464628527f1e0c28d0749d078
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-12 23:47:37 +0900

    Generic : 2번째 예시

--- java10m/practice/day1012/generic/Classname.java
+++ java10m/practice/day1012/generic/Classname.java
@@ -1,15 +1,20 @@
 package lionlike.java10m.practice.day1012.generic;
 
-public class Classname<E>{
-    private E element; // 제네릭 타입 변수
+public class Classname<F, S>{
+    private F frist; // 제네릭 타입 변수
+    private S second; // 제네릭 타입 변수
 
-    void set(E element){
-        this.element = element;
+    void set(F frist, S second){
+        this.frist = frist;
+        this.second = second;
     }
 
-    E get(){
-        return element;
+    public F getFrist() {
+        return frist;
     }
 
+    public S getSecond() {
+        return second;
+    }
 
 }
