a783a84f474670a48941d562571c32594911af3b
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-14 10:28:43 +0900

    InsertionSort 삽입정렬 마무리 1. for문 추가

--- java10m/practice/day1014/insertionsort/InsertionSort1.java
+++ java10m/practice/day1014/insertionsort/InsertionSort1.java
@@ -3,32 +3,29 @@ package lionlike.java10m.practice.day1014.insertionsort;
 import java.util.Arrays;
 
 public class InsertionSort1 {
-    public int[] Sort1(int[] arr, int j){
-        // i = 1 arr[1] vs arr[0]
-
-        // i = 2 arr[2] vs arr[1]
-        // i = 1 arr[1] vs arr[0]
-
-        int i = j;
-        int temp;
-        if (arr[i] < arr[i-1]) { //arr[1] < arr[0]
-            temp = arr[i-1]; // 0 <-5
-            arr[i-1] = arr[i];
-            arr[i] = temp;
+    public int[] Sort1(int[] arr){
+        for (int i = 1; i< arr.length; i++) {
+            for (int j = i; j>0; j--) {
+                if (arr[j] < arr[j-1]) { //arr[1] < arr[0]
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
         }
         return arr;
     }
     public static void main(String[] args) {
-        int[] arr1 = {8,5,6,2,4};
+        int[] num = {7,3,2,8,9,4,6,1,5};
         InsertionSort1 is = new InsertionSort1();
 
         // 1번째 index
-        int[] result1 = is.Sort1(arr1,1);
+        int[] result1 = is.Sort1(num);
         System.out.println(Arrays.toString(result1));
 
         // 2번째 index
         int[] arr2 = {5,8,6,2,4};
-        int[] result2 = is.Sort1(arr2,2);
+        int[] result2 = is.Sort1(arr2);
 
     }
 }
