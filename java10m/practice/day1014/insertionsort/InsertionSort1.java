8cfb1c87e41cbb00a9aa6241ca1d548dc4d63323
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-14 09:40:37 +0900

    first commit : InsertionSort 삽입정렬

---
+++ java10m/practice/day1014/insertionsort/InsertionSort1.java
@@ -0,0 +1,18 @@
+package lionlike.java10m.practice.day1014.insertionsort;
+
+import java.util.Arrays;
+
+public class InsertionSort1 {
+    public int[] Sort(int[] arr){
+        int n = 1;
+
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        int[] arr = {8,5,6,2,4};
+
+        InsertionSort is = new InsertionSort();
+        int[] result = is.Sort(arr);
+        System.out.println(Arrays.toString(result));
+    }
+}
