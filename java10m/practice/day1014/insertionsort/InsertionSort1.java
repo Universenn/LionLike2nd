a618100859253750e950d953f37c5050ff828b05
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-14 10:17:26 +0900

    InsertionSort 삽입정렬 1. j 추가

--- java10m/practice/day1014/insertionsort/InsertionSort1.java
+++ java10m/practice/day1014/insertionsort/InsertionSort1.java
@@ -3,23 +3,32 @@ package lionlike.java10m.practice.day1014.insertionsort;
 import java.util.Arrays;
 
 public class InsertionSort1 {
-    public int[] Sort1(int[] arr){
-        int i = 1;
+    public int[] Sort1(int[] arr, int j){
+        // i = 1 arr[1] vs arr[0]
+
+        // i = 2 arr[2] vs arr[1]
+        // i = 1 arr[1] vs arr[0]
+
+        int i = j;
         int temp;
         if (arr[i] < arr[i-1]) { //arr[1] < arr[0]
             temp = arr[i-1]; // 0 <-5
             arr[i-1] = arr[i];
             arr[i] = temp;
-
         }
-
         return arr;
     }
     public static void main(String[] args) {
-        int[] arr = {8,5,6,2,4};
-
+        int[] arr1 = {8,5,6,2,4};
         InsertionSort1 is = new InsertionSort1();
-        int[] result = is.Sort1(arr);
-        System.out.println(Arrays.toString(result));
+
+        // 1번째 index
+        int[] result1 = is.Sort1(arr1,1);
+        System.out.println(Arrays.toString(result1));
+
+        // 2번째 index
+        int[] arr2 = {5,8,6,2,4};
+        int[] result2 = is.Sort1(arr2,2);
+
     }
 }
