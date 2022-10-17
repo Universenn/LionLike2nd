ae1b7e809292952c12bc69c7272eb0da1dde2d43
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-17 09:11:13 +0900

    별찍기

---
+++ java10m/practice/week3/day1017/algorithms/RightTriangle.java
@@ -0,0 +1,12 @@
+package lionlike.java10m.practice.week3.day1017.algorithms;
+
+import java.util.Scanner;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        System.out.printf("*");
+        System.out.printf("**");
+        System.out.printf("***");
+        System.out.printf("****");
+    }
+}
