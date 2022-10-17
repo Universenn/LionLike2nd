8a58b85f9156de833d9475ffeadad817581a48e7
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-17 09:28:09 +0900

    별찍기 직각삼각형

--- java10m/practice/week3/day1017/algorithms/RightTriangle.java
+++ java10m/practice/week3/day1017/algorithms/RightTriangle.java
@@ -3,10 +3,21 @@ package lionlike.java10m.practice.week3.day1017.algorithms;
 import java.util.Scanner;
 
 public class RightTriangle {
+    public void printStar(int num){
+        String star = "*";
+        for (int i=0; i<num; i++){
+            for (int j=0; j<=i; j++){
+                System.out.printf("%s",star);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        System.out.printf("*");
-        System.out.printf("**");
-        System.out.printf("***");
-        System.out.printf("****");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        RightTriangle rt = new RightTriangle();
+
+        rt.printStar(num);
     }
 }
