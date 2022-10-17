1842f2cf8b19b86969d0724210f14925e7a5f3b7
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-17 09:35:54 +0900

    (활용)@, # 원하는 문자 찍기

--- java10m/practice/week3/day1017/algorithms/RightTriangle.java
+++ java10m/practice/week3/day1017/algorithms/RightTriangle.java
@@ -3,15 +3,23 @@ package lionlike.java10m.practice.week3.day1017.algorithms;
 import java.util.Scanner;
 
 public class RightTriangle {
+    private String letter = "*";
+
+    public RightTriangle() {
+    }
+    public RightTriangle(String letter) {
+        this.letter = letter;
+    }
+
     public void printStar(int num){
-        String star = "*";
         for (int i=0; i<num; i++){
             for (int j=0; j<=i; j++){
-                System.out.printf("%s",star);
+                System.out.printf("%s",letter);
             }
             System.out.println();
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
