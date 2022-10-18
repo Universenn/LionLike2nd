198d9b96b1563c416fbe943520a57ce93f9fc29e
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-18 09:18:15 +0900

    n개 정사각형 만들기

---
+++ java10m/practice/week3/day1018/Square.java
@@ -0,0 +1,22 @@
+package lionlike.java10m.practice.week3.day1018;
+
+import java.util.Scanner;
+
+public class Square {
+    public void squarePrint(int num){
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        Square square = new Square();
+        square.squarePrint(num);
+
+    }
+}
