5a5eb35a4a6959a156ced0b82230294d8537d77a
Universenn <dnwndhks98@gmail.com>
2022-10-31 21:04:54 +0900

    HashSet 연습 2 lotto 중복 제거 했지만 의도와 다름

--- java11m/week1/day1031/PrHashSet.java
+++ java11m/week1/day1031/PrHashSet.java
@@ -6,6 +6,23 @@ import java.util.Iterator;
 
 public class PrHashSet {
 
+    public int[] lotto() {
+        int[] arr = new int[6];
+
+        HashSet<Integer> loto = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random()*45+1);
+        }
+
+        for (int i : arr) {
+            loto.add(i);
+        }
+        System.out.println(loto);
+
+        return arr;
+    }
+
     public static void main(String[] args) {
         Object[] objects = {1,"1",2,2,3,3,3,4,4};
         HashSet<Object> obArr = new HashSet<>();
@@ -18,5 +35,7 @@ public class PrHashSet {
             System.out.println(o);
         }
 
+        PrHashSet phs = new PrHashSet();
+        System.out.println(Arrays.toString(phs.lotto()));
     }
 }
